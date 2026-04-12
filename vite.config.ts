94fbf814d01730b0f2611ed8a3c8a2778ae56c90
Matheus Albuquerque <land@ythecombinator.space>
2026-04-12 21:35:20 +0200

    build(package): migrate vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-// import preact from "@preact/preset-vite";
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  // plugins: [preact()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      '@components': resolve(__dirname, './src/components'),
-      '@utils': resolve(__dirname, './src/utils'),
+      '@components': resolve(import.meta.dirname, './src/components'),
+      '@utils': resolve(import.meta.dirname, './src/utils'),
     },
   },
 });
